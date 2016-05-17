83968911e9bb70a28da85c5014cb3a16dd8bb491
Jason Ball <jasonlukeball@me.com>
2016-05-17 21:42:24 +1000

    Add Unique Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'random_data'
 # Decided to use Faker instead
 
+Post.find_or_create_by(title: "Unique Post Title", body: "Unique Post Body")
+
 50.times do
   Post.create!(
         title:  Faker::Hipster.sentence,
