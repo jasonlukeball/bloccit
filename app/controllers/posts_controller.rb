90d0df0496645b4a95be5045102430ee695a17c2
Jason Ball <jasonlukeball@me.com>
2016-05-18 21:41:10 +1000

    Overwrite every 5th post title as SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @posts = @posts.each do |post|
+      if post.id % 5 == 0
+        post.title = "SPAM"
+        post.save
+      end
+    end
+    @posts = Post.all
   end
 
   def new
