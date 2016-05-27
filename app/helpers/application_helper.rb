dfafeaf6401bcdc33049113903031b537dac4780
Jason Ball <jasonlukeball@me.com>
2016-05-28 09:10:02 +1000

    Add application helper methods to display vaildation errors for topics and posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,23 @@ module ApplicationHelper
     content_tag :div, capture(&block), class: css_class
   end
 
+
+  def display_errors(heading, errors_list)
+    h4 = content_tag :h4, heading
+    errors_list = content_tag :ul do
+      errors_list.each { |list_item| concat content_tag :li, list_item}.join
+    end
+    content_tag :div, h4.concat(errors_list), class: 'alert alert-danger'
+  end
+
+  def display_errors_v2(heading, errors_list)
+    output = "<div class= 'alert alert-danger'>"
+    output += "<h4>#{heading}</h4>"
+    output += "<ul>"
+    errors_list.each { |list_item| output+= "<li>#{list_item}</li>" }
+    output += "</ul>"
+    output += "</div>"
+    output.html_safe #html_safe returns as HTML rather than plain text
+  end
+
 end
