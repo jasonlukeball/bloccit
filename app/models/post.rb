c300f6beef9fa4e3ee35e5444eee639036c97f51
Jason Ball <jasonlukeball@me.com>
2016-05-31 22:35:43 +1000

    Add Post scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,26 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
-
   validates :title, length: { minimum:  5}, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
 
   after_create :censor
+
+
+  # SCOPES
+  default_scope { order('created_at DESC') }
+
+  def self.ordered_by_title
+    order('title ASC')
+  end
+
+
+  def self.ordered_by_reverse_created_at
+    order('created_at ASC')
+  end
+
 
   def censor
     update_attribute(:title, "SPAM") if self.id % 5 == 0
